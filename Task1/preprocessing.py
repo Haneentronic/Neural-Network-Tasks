9292c881eb02901c0d1df3d6fb94d4bec676b8b0
Ruqaiyah <ruqaiyah.mohammed25@gmail.com>
2023-10-28 13:33:59 +0200

    feat: normalization added

--- Task1/preprocessing.py
+++ Task1/preprocessing.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
 
 
 class PreProcessing:
@@ -10,6 +11,7 @@ class PreProcessing:
         self.x_test = None
         self.y_train = None
         self.y_test = None
+        self.scaler = None
 
     def read_data(self, filename, features, classes):
         self.x = pd.read_csv(filename)
@@ -20,3 +22,10 @@ class PreProcessing:
     def split_data(self, split_rate):
         self.x_train, self.x_test, self.y_train, self.y_test =\
             train_test_split(self.x, self.y, test_size=split_rate/100, stratify=self.y.values)
+
+    def normalize_train_data(self):
+        self.scaler = MinMaxScaler()
+        self.x_train = self.scaler.fit_transform(self.x_train)
+
+    def normalize_test_data(self):
+        self.x_test = self.scaler.transform(self.x_test)
