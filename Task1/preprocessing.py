c20c2e99b956fe8cd24eb6c775a4b8f000312612
Ruqaiyah <ruqaiyah.mohammed25@gmail.com>
2023-10-29 02:21:52 +0200

    feat: null and encoding

--- Task1/preprocessing.py
+++ Task1/preprocessing.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
 
 
 class PreProcessing:
@@ -17,11 +19,18 @@ class PreProcessing:
         self.x = pd.read_csv(filename)
         self.x = self.x.loc[self.x[self.x.columns[-1]].isin(classes)]
         self.y = self.x.iloc[:, -1:]
+        le = LabelEncoder()
+        self.y = le.fit_transform(self.y.values)
+        self.y = (self.y * 2) - 1
         self.x = self.x[features]
 
     def split_data(self, split_rate):
         self.x_train, self.x_test, self.y_train, self.y_test =\
             train_test_split(self.x, self.y, test_size=split_rate/100, stratify=self.y.values)
+
+    def null_handel(self):
+        if 'MinorAxisLength' in self.x_train.columns:
+            self.x_train.fillna(np.mean(self.x_train['MinorAxisLength']), inplace=True)
 
     def normalize_train_data(self):
         self.scaler = MinMaxScaler()
@@ -29,3 +38,4 @@ class PreProcessing:
 
     def normalize_test_data(self):
         self.x_test = self.scaler.transform(self.x_test)
+
