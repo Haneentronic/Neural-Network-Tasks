635049730c826947122e2f871bfc848c1355a747
Haneen Ibrahim <91877743+HaneenIbrahim2@users.noreply.github.com>
2023-11-03 16:47:21 +0200

    Confusion matrix and accuracy

--- Task1/perceptron.py
+++ Task1/perceptron.py
@@ -1,6 +1,9 @@
 import numpy as np
 import random
 import pandas as pd
+import matplotlib.pyplot as plt
+from preprocessing import PreProcessing
+
 
 class Perceptron:
     def __init__(self, data, ephocs, learningrate, bais):
@@ -10,6 +13,7 @@ class Perceptron:
         self.bais = bais
         self.w = None
         self.prediction = None
+
 
     def perceptron_train(self):
         if self.bais:
@@ -25,5 +29,54 @@ class Perceptron:
                     self.w = self.w + (np.dot(loss * self.learningrate, self.data.x_train.iloc[i, :]))
 
     def perceptron_test(self):
-        self.prediction = np.sign(np.dot(self.data.x_test, self.w.T))
+        self.prediction = np.array(np.sign(np.dot(self.data.x_test, self.w.T)))
 
+
+    def ploting(self):
+        plt.scatter(self.data.x_train.iloc[:, 0], self.data.x_train.iloc[:, 1], c=self.data.y_train.iloc[:, -1])
+        x = np.linspace(self.data.x_train.iloc[:, 0].min(), self.data.x_train.iloc[:, 0].max(), 100)
+        y = -(self.w[0] * x + self.bais) / self.w[1]
+        plt.plot(x, y, color='red')
+        plt.show()
+
+    def confusion_matrix(self):
+        # Get the unique classes
+        classes = np.unique(np.concatenate((np.array(self.data.y_test.iloc[:,0]), self.prediction)))
+
+        # Initialize the confusion matrix
+        cm = np.zeros((len(classes), len(classes)))
+
+        # Fill the confusion matrix
+        for i in range(len(np.array(self.data.y_test.iloc[:,0]))):
+            true_class = np.where(classes == np.array(self.data.y_test.iloc[:,0])[i])[0][0]
+            pred_class = np.where(classes == self.prediction[i])[0][0]
+            cm[true_class][pred_class] += 1
+
+        return cm
+
+    def accuracy_score(self):
+        # Calculate the number of correct predictions
+        correct = sum(np.array(self.data.y_test.iloc[:,0]) == self.prediction)
+
+        # Calculate the total number of predictions
+        total = len(np.array(self.data.y_test.iloc[:,0]))
+
+        # Calculate the accuracy score
+        accuracy = correct / total
+
+        return accuracy
+
+
+# preprocessing = PreProcessing()
+# preprocessing.read_data("Dry_Bean_Dataset.csv", ["Perimeter", "Area"], ["BOMBAY", "CALI"])
+# preprocessing.split_data(40)
+# preprocessing.null_handel()
+# o = Perceptron(preprocessing, 100, 0.1, 0)
+# o.perceptron_train()
+# o.perceptron_test()
+# # o.confusion_matrix(o.data.y_test,o.prediction)
+# print(type(np.array(o.data.y_test.loc[:,0])))
+# print(type(o.prediction))
+# print(o.confusion_matrix())
+# print(o.accuracy_score())
+# o.ploting()
