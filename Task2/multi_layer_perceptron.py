685e6bead2012ab859ef61c5e0b071c903dba165
Ruqaiyah <ruqaiyah.mohammed25@gmail.com>
2023-11-24 20:27:15 +0200

    feat: forward propagation function

--- Task2/multi_layer_perceptron.py
+++ Task2/multi_layer_perceptron.py
@@ -2,6 +2,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 
+
 class NeuralNetwork:
     def __init__(self, input_size, hidden_layers, neurons_hidden, activation, learning_rate, epochs):
         self.input_size = input_size
@@ -10,7 +11,9 @@ class NeuralNetwork:
         self.activation = activation
         self.learning_rate = learning_rate
         self.epochs = epochs
-        self.weights, self.bias = self._initialize_weights_and_bias()
+        self.weights = None
+        self.bias = None
+        # self.weights, self.bias = self._initialize_weights_and_bias()
 
     def _initialize_weights_and_bias(self):
         weights = []
@@ -37,7 +40,13 @@ class NeuralNetwork:
             return 1 - np.tanh(x) ** 2
 
     def _forward_propagation(self, x):
-       pass
+        output = []
+        activated = x
+        for layer_number in range(self.hidden_layers + 1):
+            net = np.dot(activated, np.array(self.weights[layer_number]).T) + self.bias[layer_number]
+            activated = self._activation_function(net)
+            output.extend(activated)
+        return output
 
     def _backward_propagation(self, x, y):
         layers = self._forward_propagation(x)
@@ -72,3 +81,9 @@ class NeuralNetwork:
     def predict(self, X_test):
         pass
 
+
+# testing forward propagation function on lecture 4 example
+obj = NeuralNetwork(2, 1, 2, "sigmoid", 1, 1)
+obj.weights = np.array([[[0.21, 0.15], [-0.4, 0.1]], [[-0.2, 0.3]]], dtype=object) # sub array per layer containing sub array per neuron
+obj.bias = np.array([[-0.3, 0.25], -0.4], dtype=object)  # subarray per layer
+print(obj._forward_propagation([0, 0]))
